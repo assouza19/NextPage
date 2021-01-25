e0a0fd7880f72f5b53fc353bea1b92d2643fb894
Aline Silva <aline.silva@picpay.com>
2021-01-25 12:31:01 -0300

    Ajustes na camada de Data

--- app/src/main/java/com/br/nextpage/data/repository/BookRepositoryImpl.kt
+++ app/src/main/java/com/br/nextpage/data/repository/BookRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.br.nextpage.data.repository
 
 import com.br.nextpage.data.dataSource.BookLocalDataSource
 import com.br.nextpage.data.dataSource.BookRemoteDataSource
+import com.br.nextpage.data.models.BookDetailResponse
+import com.br.nextpage.data.models.BookResponseItem
 import com.br.nextpage.domain.models.Book
 import com.br.nextpage.domain.models.BookDetail
 import com.br.nextpage.domain.repository.BookRepository
@@ -12,11 +14,37 @@ internal class BookRepositoryImpl(
 ) : BookRepository {
 
     override suspend fun getBooks(token: String): List<Book> {
-        return remoteDataSource.getBooks(token)
+        return mapToDomainList(remoteDataSource.getBooks(token))
     }
 
     override suspend fun getBookDetail(idBook: String, token: String): BookDetail {
-        return remoteDataSource.getBookDetail(idBook, token)
+        return mapToDomainDetail(remoteDataSource.getBookDetail(idBook, token))
+    }
+
+    private fun mapToDomainDetail(bookDetail: BookDetailResponse): BookDetail {
+        return BookDetail(
+            idBook = bookDetail.idBook,
+            author = bookDetail.author,
+            cover = bookDetail.cover,
+            publishing = bookDetail.publishing,
+            title = bookDetail.title,
+            year = bookDetail.year,
+            category = bookDetail.category
+        )
+    }
+
+    private fun mapToDomainList(books: List<BookResponseItem>): List<Book> {
+        return books.map {
+            Book(
+                idBook = it.idBook,
+                author = it.author,
+                cover = it.cover,
+                publishing = it.publishing,
+                title = it.title,
+                year = it.year,
+                category = it.category
+            )
+        }
     }
 }
 
--- app/src/main/java/com/br/nextpage/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/br/nextpage/data/repository/UserRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.br.nextpage.data.repository
 
-import androidx.core.content.edit
 import com.br.nextpage.data.dataSource.UserDataSource
 import com.br.nextpage.data.dataSource.UserLocalDataSource
+import com.br.nextpage.data.models.LoginResponse
 import com.br.nextpage.domain.models.Login
+import com.br.nextpage.domain.models.Token
 import com.br.nextpage.domain.repository.UserRepository
 import retrofit2.Response
 
@@ -13,7 +14,17 @@ internal class UserRepositoryImpl(
 ) : UserRepository {
 
     override suspend fun login(): Login {
-        return dataSource.login().mapToDomain()
+        return mapToDomain(dataSource.login())
+    }
+
+    private fun mapToDomain(login: Response<LoginResponse>): Login {
+        return Login(
+            token = Token(
+                access_token = login.body()?.token?.access_token.orEmpty(),
+                token_type = login.body()?.token?.token_type.orEmpty()
+            ),
+            url = login.body()?.url.orEmpty()
+        )
     }
 
     override fun saveToken(token: String) {
@@ -24,5 +35,3 @@ internal class UserRepositoryImpl(
         return localDataSource.getToken()
     }
 }
-
-private fun <T> T?.orNull() = null
